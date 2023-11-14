67ec3371bed927d2dc3c755420d300381283172a
Davi Borges <davifelipe_300@hotmail.com>
2023-11-14 18:14:47 -0300

    Aprimorada a consulta por ID com Include para carregar ListaAtividades associada ao Médico.

--- e-AgendaMedica.Infra.Orm/ModuloMedico/RepositorioMedicoOrm.cs
+++ e-AgendaMedica.Infra.Orm/ModuloMedico/RepositorioMedicoOrm.cs
@@ -1,6 +1,8 @@
 ﻿using e_Agenda.Dominio.Compartilhado;
+using e_AgendaMedica.Dominio.ModuloAtividade;
 using e_AgendaMedica.Dominio.ModuloMedico;
 using e_AgendaMedica.Infra.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
 
 namespace e_AgendaMedica.Infra.Orm.ModuloMedico
 {
@@ -13,6 +15,13 @@ namespace e_AgendaMedica.Infra.Orm.ModuloMedico
         public List<Medico> SelecionarMuitos(List<Guid> idsMedicosSelecionados)
         {
             return registros.Where(medicos => idsMedicosSelecionados.Contains(medicos.Id)).ToList();
+        }
+
+        public override async Task<Medico> SelecionarPorIdAsync(Guid id)
+        {
+            return await registros
+                .Include(x => x.ListaAtividades)
+                .SingleOrDefaultAsync(x => x.Id == id);
         }
     }
 }
