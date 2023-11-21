f5afe3a8483cc6d909779851b829f4053aff8120
Davi Borges <davifelipe_300@hotmail.com>
2023-11-21 18:19:04 -0300

    adiciona view model

--- e-AgendaMedica.WebApi/ViewModels/ModuloAtividade/InserirAtividadeViewModel.cs
+++ e-AgendaMedica.WebApi/ViewModels/ModuloAtividade/InserirAtividadeViewModel.cs
@@ -6,13 +6,10 @@ namespace e_AgendaMedica.WebApi.ViewModels.ModuloAtividade
     public class InserirAtividadeViewModel
     {
 
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Data { get; set; }
 
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:mm:ss}")]
         public string HorarioTermino { get; set; }
 
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:mm:ss}")]
         public string HorarioInicio { get; set; }
 
         public string Paciente { get; set; }
